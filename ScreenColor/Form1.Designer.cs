688ad0e023f8e362d55c9c8bce4444eaacf9701d
DHRUV PATEL <109400107+dhruvdxb@users.noreply.github.com>
2024-08-03 09:52:06 +0530

    Add files via upload

---
+++ ScreenColor/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ScreenColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.color = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.HoldMousebtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelRed = new System.Windows.Forms.Label();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // color
+            // 
+            this.color.AutoSize = true;
+            this.color.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.color.Location = new System.Drawing.Point(48, 47);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(83, 24);
+            this.color.TabIndex = 0;
+            this.color.Text = "HasCode";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(125, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // HoldMousebtn
+            // 
+            this.HoldMousebtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(175)))), ((int)(((byte)(175)))));
+            this.HoldMousebtn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HoldMousebtn.ForeColor = System.Drawing.Color.White;
+            this.HoldMousebtn.Location = new System.Drawing.Point(32, 241);
+            this.HoldMousebtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.HoldMousebtn.Name = "HoldMousebtn";
+            this.HoldMousebtn.Size = new System.Drawing.Size(160, 115);
+            this.HoldMousebtn.TabIndex = 2;
+            this.HoldMousebtn.Text = "Hold Mouse";
+            this.HoldMousebtn.UseVisualStyleBackColor = false;
+            this.HoldMousebtn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HoldMousebtn_MouseDown);
+            this.HoldMousebtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.HoldMousebtn_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(344, 47);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 309);
+            this.panel1.TabIndex = 3;
+            // 
+            // labelRed
+            // 
+            this.labelRed.AutoSize = true;
+            this.labelRed.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRed.Location = new System.Drawing.Point(79, 106);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(52, 24);
+            this.labelRed.TabIndex = 4;
+            this.labelRed.Text = "Red :";
+            // 
+            // labelGreen
+            // 
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGreen.Location = new System.Drawing.Point(65, 148);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(71, 24);
+            this.labelGreen.TabIndex = 5;
+            this.labelGreen.Text = "Green :";
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBlue.Location = new System.Drawing.Point(74, 189);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(57, 24);
+            this.labelBlue.TabIndex = 6;
+            this.labelBlue.Text = "Blue :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(58)))), ((int)(((byte)(118)))));
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(652, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2, 3, 0, 0);
+            this.label1.Size = new System.Drawing.Size(28, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(58)))), ((int)(((byte)(118)))));
+            this.label2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(2, 3, 0, 0);
+            this.label2.Size = new System.Drawing.Size(701, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DIGITAL COLOUR METER";
+            // 
+            // copyButton
+            // 
+            this.copyButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(58)))), ((int)(((byte)(118)))));
+            this.copyButton.ForeColor = System.Drawing.Color.White;
+            this.copyButton.Location = new System.Drawing.Point(207, 241);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(120, 115);
+            this.copyButton.TabIndex = 9;
+            this.copyButton.Text = "Copy Color Code";
+            this.copyButton.UseVisualStyleBackColor = false;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(681, 390);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelBlue);
+            this.Controls.Add(this.labelGreen);
+            this.Controls.Add(this.labelRed);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.HoldMousebtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.color);
+            this.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label color;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button HoldMousebtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button copyButton;
+    }
+}
---
+++ ScreenColor/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace ScreenColor
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        private static extern int GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        bool HoldMousebtnPressed = false;
+        bool isColorLocked = false;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread thread = new Thread(BackGroundThread) { IsBackground = true };
+            thread.Start();
+        }
+
+        void BackGroundThread()
+        {
+            while (true)
+            {
+                if (HoldMousebtnPressed && !isColorLocked)
+                {
+                    Point cursorPosition = Cursor.Position;
+                    Color selectedColor = GetColorAt(cursorPosition);
+                    string colorHtml = ColorTranslator.ToHtml(selectedColor);
+
+                   
+                    this.Invoke(new Action(() =>
+                    {
+                        panel1.BackColor = selectedColor;
+                        textBox1.Text = colorHtml;
+
+                    
+                        int red = selectedColor.R;
+                        int green = selectedColor.G;
+                        int blue = selectedColor.B;
+
+                    
+                        labelRed.Text = $"Red: {red}";
+                        labelGreen.Text = $"Green: {green}";
+                        labelBlue.Text = $"Blue: {blue}";
+                    }));
+                }
+            }
+        }
+
+        Color GetColorAt(Point location)
+        {
+           
+            IntPtr hdc = GetDC(IntPtr.Zero);
+
+           
+            int pixelColor = GetPixel(hdc, location.X, location.Y);
+
+            
+            ReleaseDC(IntPtr.Zero, hdc);
+
+          
+            int red = (pixelColor & 0x00FF0000) >> 16;
+            int green = (pixelColor & 0x0000FF00) >> 8;
+            int blue = (pixelColor & 0x000000FF);
+
+           
+            return Color.FromArgb(red, green, blue);
+        }
+
+        private void HoldMousebtn_MouseDown(object sender, MouseEventArgs e)
+        {
+            HoldMousebtnPressed = true;
+            Cursor = Cursors.Cross;
+
+        }
+
+        private void HoldMousebtn_MouseUp(object sender, MouseEventArgs e)
+        {
+            HoldMousebtnPressed = false;
+            Cursor = Cursors.Default;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.L))
+            {
+                isColorLocked = !isColorLocked;
+                MessageBox.Show(isColorLocked ? "Color locked." : "Color unlocked."); 
+                return true; 
+            }
+            return base.ProcessCmdKey(ref msg, keyData); 
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            if (panel1.BackColor != null)
+            {
+                Color selectedColor = panel1.BackColor;
+                string colorHtml = ColorTranslator.ToHtml(selectedColor);
+
+              
+                int red = selectedColor.R;
+                int green = selectedColor.G;
+                int blue = selectedColor.B;
+
+              
+                string clipboardText = $"Color Information:\n" +
+                                       $"HEX: {colorHtml}\n" +
+                                       $"RGB: {red}, {green}, {blue}";
+
+                Clipboard.SetText(clipboardText);
+                MessageBox.Show("Color information copied to clipboard.");
+            }
+        }
+    }
+}
+
